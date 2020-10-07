7c08adecb96d362a2f4034d337172ce698c18626
Michael Steinbaugh <mike@steinbaugh.com>
2020-10-07 08:51:15 -0400

    Simplify NAMESPACE requirements

--- R/imports.R
+++ R/imports.R
@@ -2,7 +2,7 @@
 #'
 #' @importFrom AcidBase appendToBody
 #' @importFrom S4Vectors DataFrame decode merge na.omit order setdiff unique
-#' @importFrom goalie areDisjointSets assert hasColnames hasDims hasLength
-#'   hasRownames isAny isCharacter isNumber isSubset
+#' @importFrom goalie allAreAtomic areDisjointSets assert hasColnames hasDims
+#'   hasLength hasRownames isAny isCharacter isNumber isSubset
 #' @importFrom methods setMethod signature
 NULL
