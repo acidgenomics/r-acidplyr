d83e5f7858090a1424de3636e69554b70fa430c9
Michael Steinbaugh <mike@steinbaugh.com>
2020-10-07 08:59:07 -0400

    Update NAMESPACE

--- R/imports.R
+++ R/imports.R
@@ -2,8 +2,9 @@
 #'
 #' @importFrom AcidBase appendToBody
 #' @importFrom S4Vectors DataFrame decode merge na.omit order setdiff unique
+#' @importFrom cli cli_alert cli_alert_info
 #' @importFrom goalie allAreAtomic areDisjointSets assert bapply hasColnames
-#'   hasDims hasLength hasRownames isAny isCharacter isFlag isNumber isString
-#'   isSubset
+#'   hasDims hasLength hasRownames isAny isCharacter isFlag isInt isNumber
+#'   isString isSubset
 #' @importFrom methods setMethod signature
 NULL
