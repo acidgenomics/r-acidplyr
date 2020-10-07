787efb37e8f8c5b2d18f12d2057143e545ef37a6
Michael Steinbaugh <mike@steinbaugh.com>
2020-10-07 08:23:33 -0400

    Migrate methods from basejump

--- R/imports.R
+++ R/imports.R
@@ -1,5 +1,6 @@
 #' @importClassesFrom S4Vectors DataFrame
 #'
+#' @importFrom AcidBase appendToBody
 #' @importFrom S4Vectors merge na.omit order setdiff unique
 #' @importFrom goalie areDisjointSets assert hasLength hasRownames isSubset
 #' @importFrom methods setMethod signature
