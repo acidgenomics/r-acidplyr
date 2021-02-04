cd0b4d2d5189ed387682d02746655d70d73f779f
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-04 11:16:44 -0500

    Update package title in description

--- R/package.R
+++ R/package.R
@@ -1,6 +1,23 @@
 #' AcidPlyr
 #'
-#' A grammar of S4 class data manipulation.
+#' A dplyr-like grammar for data manipulation of S4 classes defined
+#' in S4Vectors.
 #'
 #' @keywords internal
 "_PACKAGE"
+
+
+
+#' @importClassesFrom S4Vectors DataFrame
+#'
+#' @importFrom AcidBase appendToBody
+#' @importFrom AcidCLI alert alertInfo
+#' @importFrom AcidGenerics decode merge na.omit order setdiff unique
+#' @importFrom S4Vectors DataFrame
+#' @importFrom goalie allAreAtomic areDisjointSets assert bapply hasColnames
+#'   hasDims hasLength hasNoDuplicates hasRownames isAny isCharacter isFlag
+#'   isInt isNumber isScalar isString isSubset
+#' @importFrom methods as setMethod signature
+#' @importFrom purrr map_dfr
+#' @importFrom tibble as_tibble
+NULL
