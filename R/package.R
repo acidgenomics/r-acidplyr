fbd54bce8cec321e35685c20b0ccb8ae77176c42
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-11 16:25:43 -0500

    Simplify NAMESPACE

--- R/package.R
+++ R/package.R
@@ -4,20 +4,16 @@
 #' in S4Vectors.
 #'
 #' @keywords internal
-"_PACKAGE"
-
-
-
-#' @importClassesFrom S4Vectors DataFrame
+#'
+#' @importClassesFrom AcidGenerics DataFrame
 #'
 #' @importFrom AcidBase appendToBody
 #' @importFrom AcidCLI alert alertInfo
-#' @importFrom AcidGenerics decode merge na.omit order setdiff unique
-#' @importFrom S4Vectors DataFrame
+#' @importFrom AcidGenerics DataFrame decode merge na.omit order setdiff unique
 #' @importFrom goalie allAreAtomic areDisjointSets assert bapply hasColnames
 #'   hasDims hasLength hasNoDuplicates hasRownames isAny isCharacter isFlag
 #'   isInt isNumber isScalar isString isSubset
 #' @importFrom methods as setMethod signature
 #' @importFrom purrr map_dfr
 #' @importFrom tibble as_tibble
-NULL
+"_PACKAGE"
