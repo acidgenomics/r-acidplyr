76992dd490056f5bb0a2427a561d053bfd910c76
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-19 08:18:46 -0500

    Add recursive mode option to `unlistToDataFrame`

--- R/package.R
+++ R/package.R
@@ -10,7 +10,7 @@
 #' @importFrom AcidBase appendToBody
 #' @importFrom AcidCLI alert alertInfo
 #' @importFrom AcidGenerics DataFrame as.data.frame decode merge na.omit order
-#'   setdiff unique
+#'   rbind setdiff unique
 #' @importFrom goalie allAreAtomic areDisjointSets assert bapply hasColnames
 #'   hasDims hasLength hasNoDuplicates hasRownames isAny isCharacter isFlag
 #'   isInt isNumber isScalar isString isSubset
