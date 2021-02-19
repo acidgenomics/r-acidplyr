513e41df45620620cc99daaf2cb682839ad205af
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-19 14:13:01 -0500

    Rework unit test

--- R/unlistToDataFrame-methods.R
+++ R/unlistToDataFrame-methods.R
@@ -1,7 +1,3 @@
-## FIXME MOVE THIS TO PIPETTE AND REWORK INTO AS.DATAFRAME.
-
-
-
 #' @name unlistToDataFrame
 #' @inherit AcidGenerics::unlistToDataFrame
 #' @note Updated 2021-02-19.
@@ -18,21 +14,21 @@
 #' @seealso
 #' - `pipette::as.DataFrame()`.
 #' - `purrr::map_dfr()`.
-#' - `plyr::ldply()` (deprecated).
+#' - `plyr::ldply()` (*deprecated*).
 #'
 #' @examples
 #' x <- list(
-#'     a = list(
-#'         aa = seq(from = 1L, to = 3L),
-#'         bb = seq(from = 4L, to = 6L)
+#'     "a" = list(
+#'         "aa" = seq(from = 1L, to = 3L),
+#'         "bb" = seq(from = 4L, to = 6L)
 #'     ),
-#'     b = list(
-#'         cc = seq(from = 7L, to = 9L),
-#'         dd = seq(from = 10L, to = 12L)
+#'     "b" = list(
+#'         "cc" = seq(from = 7L, to = 9L),
+#'         "dd" = seq(from = 10L, to = 12L)
 #'     ),
-#'     c = list(
-#'         ee = seq(from = 13L, to = 15L),
-#'         ff = seq(from = 16L, to = 18L)
+#'     "c" = list(
+#'         "ee" = seq(from = 13L, to = 15L),
+#'         "ff" = seq(from = 16L, to = 18L)
 #'     )
 #' )
 #' print(x)
@@ -41,12 +37,19 @@
 #' y <- unlistToDataFrame(x, recursive = FALSE)
 #' print(y)
 #' ## Note that non-recursive elements must be nested down an extra level.
+#' ## FIXME RETHINK THIS.
 #' print(y[[1L]][[1L]])
 NULL
 
 
 
 ## This step handles nested S4 List elements.
+##
+## The `purrr::map_dfr()` method doesn't expect complex S4 input and can do
+## weird, unexpected things. The function will return `group`, `group_name`, and
+## `value` columns in this case, which we don't want here. So we're hardening
+## against unexpected return by not allowing complex S4 items in recursive mode.
+##
 ## Updated 2021-02-19.
 .decodeNestedList <- function(x) {
     lapply(
@@ -67,134 +70,18 @@ NULL
 
 
 ## Updated 2021-02-19.
-.unlistRecursive <- function(x) {
-    x <- .decodeNestedList(x)
-    assert(hasNames(x))
-    idCol <- "name"
-    assert(areDisjointSets(idCol, names(x)))
-    y <- map_dfr(.x = x, .f = ~.x, .id = idCol)
-    ## This doesn't get set when we bind a simple list of atomic vectors.
-    if (isSubset(idCol, colnames(y))) {
-        y[[idCol]] <- as.factor(y[[idCol]])
-    }
-    y <- as(y, "DataFrame")
-    y
-}
-
-
-
-## Updated 2021-02-19.
-.unlistNested <- function(x) {
-    scalarList <- lapply(
-        X = x,
-        FUN = function(x) {
-            assert(hasNames(x))
-            bapply(
-                X = x,
-                FUN = function(x) {
-                    isScalar(x)
-                }
-            )
-        }
-    )
-    colnames <- unique(unlist(
-        lapply(X = scalar, FUN = names),
-        recursive = FALSE,
-        use.names = FALSE
-    ))
-    assert(isCharacter(colnames))
-    scalarMat <- matrix(
-        data = TRUE,
-        nrow = length(x),
-        ncol = length(colnames),
-        dimnames = list(names(x), colnames)
-    )
-    for (i in seq_along(scalarList)) {
-        for (j in seq_along(scalarList[[i]])) {
-            rowname <- names(scalarList)[[i]]
-            colname <- names(scalarList[[i]])[[j]]
-            if (isFALSE(scalarList[[i]][[j]])) {
-                scalarMat[rowname, colname] <- FALSE
-            }
-        }
-    }
-    scalarCols <- apply(X = scalarMat, MARGIN = 2L, FUN = all)
-    complexCols <- names(which(!scalarCols))
-    ## FIXME NEED TO TEST THIS CASE.
-    if (!hasLength(complexCols)) {
-        df <- DataFrame(
-            lapply(X = x, FUN = I),
-            row.names = names(x)
-        )
-        return(df)
-    }
-    scalarCols <- names(which(scalarCols))
-
-
-
-
-    ## FIXME RETHINKT THIS APPROACH?
-    ## ASSIGN INTO EXPANDED MATRIX?
-    df <- DataFrame(matrix(
-        data = NA,
-        nrow = nrow(scalarMat),
-        ncol = ncol(scalarMat),
-        dimnames = dimnames(scalarMat)
-    ))
-
-
-    ## UGH THIS CODE IS MESSY, NEED TO RETHINK...
-    ## Collect the columns and selectively assign.
-    cols <- mapply(
-        colname = colnames(scalarMat),
-        MoreArgs = list(
-            rownames = rownames(scalarMat),
-            input = x
-        ),
-        FUN = function(colname, rownames, input) {
-            ok <- colname %in% scalarCols
-            xxx <- list()
-            for (i in seq_along(list)) {
-                xxx[[i]] <- list[[i]][[col]]
-            }
-            if (isTRUE(ok)) {
-                xxx <- unlist(xxx, recursive = FALSE, use.names = FALSE)
-            }
-            xxx
-        },
-        SIMPLIFY = FALSE,
-        USE.NAMES = FALSE
-    )
-    names(cols) <- rownames(scalarMat)
-
-    assert(
-        hasLength(nrow(y), n = 1L),
-        identical(length(x), ncol(y))
-    )
-}
-
-
-
-## Recursive mode was previous default, so keeping that intact.
-##
-## The `purrr::map_dfr()` method doesn't expect complex S4 input and can do
-## weird, unexpected things. The function will return `group`, `group_name`, and
-## `value` columns in this case, which we don't want here. So we're hardening
-## against unexpected return by not allowing complex S4 items in recursive mode.
-##
-## Updated 2021-02-19.
 `unlistToDataFrame,list` <-  # nolint
-    function(x, recursive = TRUE) {
-        assert(
-            hasLength(x),
-            hasNames(x),
-            isFlag(recursive)
-        )
-        if (isTRUE(recursive)) {
-            y <- .unlistRecursive(x)
-        } else {
-            y <- .unlistNested(x)
+    function(x) {
+        assert(hasLength(x), hasNames(x))
+        x <- .decodeNestedList(x)
+        idCol <- "name"
+        assert(areDisjointSets(idCol, names(x)))
+        y <- map_dfr(.x = x, .f = ~.x, .id = idCol)
+        ## This doesn't get set when we bind a simple list of atomic vectors.
+        if (isSubset(idCol, colnames(y))) {
+            y[[idCol]] <- as.factor(y[[idCol]])
         }
+        y <- as(y, "DataFrame")
         y
     }
 
