15172639f12c564ed6c453b1350f5e161f50de81
Michael Steinbaugh <mike@steinbaugh.com>
2021-03-02 15:59:13 -0500

    Attempt to remove dependency on tibble coercion method

--- R/package.R
+++ R/package.R
@@ -9,11 +9,11 @@
 #'
 #' @importFrom AcidCLI alert alertInfo
 #' @importFrom AcidGenerics DataFrame SimpleList as.data.frame cbind decode
-#'   do.call expand.grid lapply merge na.omit order rbind rowSums setdiff unique
+#'   do.call expand.grid lapply merge metadata metadata<- na.omit order rbind
+#'   rowSums setdiff unique
 #' @importFrom goalie allAreAtomic areDisjointSets assert bapply hasColnames
 #'   hasDims hasLength hasNames hasNoDuplicates hasRownames isAny isCharacter
 #'   isFlag isInt isNumber isScalar isScalarAtomic isString isSubset
 #' @importFrom methods as is setMethod signature
 #' @importFrom purrr map_dfr
-#' @importFrom tibble as_tibble
 "_PACKAGE"
