ba9a522daec29e38bb8b215029b1eae17e272c57
Michael Steinbaugh <mike@steinbaugh.com>
2021-04-27 16:52:10 -0400

    Prepare release

--- R/reexports.R
+++ R/reexports.R
@@ -1,11 +1,3 @@
 #' @importFrom AcidGenerics DataFrame
 #' @export
 AcidGenerics::DataFrame
-
-#' @importFrom purrr map_df
-#' @export
-purrr::map_df
-
-#' @importFrom purrr map_dfr
-#' @export
-purrr::map_dfr
