66c43427b97eed7340bc1a4827f5bcfc504452fb
Michael Steinbaugh <mike@steinbaugh.com>
2021-10-14 16:25:00 -0400

    Improve unit test

--- tests/testthat/test-join.R
+++ tests/testthat/test-join.R
@@ -11,9 +11,9 @@ by <- "name"
 test_that("innerJoin", {
     object <- innerJoin(x = x, y = y, by = by)
     expected <- DataFrame(
-        name = c("John", "Paul"),
-        band = c("Beatles", "Beatles"),
-        plays = c("guitar", "bass"),
+        "name" = c("John", "Paul"),
+        "band" = c("Beatles", "Beatles"),
+        "plays" = c("guitar", "bass"),
         row.names = c("John", "Paul")
     )
     expect_identical(object, expected)
@@ -22,9 +22,9 @@ test_that("innerJoin", {
 test_that("leftJoin", {
     object <- leftJoin(x = x, y = y, by = by)
     expected <- DataFrame(
-        name = c("Mick", "John", "Paul"),
-        band = c("Stones", "Beatles", "Beatles"),
-        plays = c(NA, "guitar", "bass"),
+        "name" = c("Mick", "John", "Paul"),
+        "band" = c("Stones", "Beatles", "Beatles"),
+        "plays" = c(NA, "guitar", "bass"),
         row.names = c("Mick", "John", "Paul")
     )
     expect_identical(object, expected)
@@ -33,9 +33,9 @@ test_that("leftJoin", {
 test_that("rightJoin", {
     object <- rightJoin(x = x, y = y, by = by)
     expected <- DataFrame(
-        name = c("John", "Paul", "Keith"),
-        plays = c("guitar", "bass", "guitar"),
-        band = c("Beatles", "Beatles", NA),
+        "name" = c("John", "Paul", "Keith"),
+        "plays" = c("guitar", "bass", "guitar"),
+        "band" = c("Beatles", "Beatles", NA),
         row.names = c("John", "Paul", "Keith")
     )
     expect_identical(object, expected)
@@ -44,9 +44,9 @@ test_that("rightJoin", {
 test_that("fullJoin", {
     object <- fullJoin(x = x, y = y, by = by)
     expected <- DataFrame(
-        name = c("Mick", "John", "Paul", "Keith"),
-        band = c("Stones", "Beatles", "Beatles", NA),
-        plays = c(NA, "guitar", "bass", "guitar"),
+        "name" = c("Mick", "John", "Paul", "Keith"),
+        "band" = c("Stones", "Beatles", "Beatles", NA),
+        "plays" = c(NA, "guitar", "bass", "guitar"),
         row.names = c("Mick", "John", "Paul", "Keith")
     )
     expect_identical(object, expected)
@@ -55,8 +55,8 @@ test_that("fullJoin", {
 test_that("semiJoin", {
     object <- semiJoin(x = x, y = y, by = by)
     expected <- DataFrame(
-        name = c("John", "Paul"),
-        band = c("Beatles", "Beatles"),
+        "name" = c("John", "Paul"),
+        "band" = c("Beatles", "Beatles"),
         row.names = c("John", "Paul")
     )
     expect_identical(object, expected)
@@ -65,8 +65,8 @@ test_that("semiJoin", {
 test_that("antiJoin", {
     object <- antiJoin(x = x, y = y, by = by)
     expected <- DataFrame(
-        name = "Mick",
-        band = "Stones",
+        "name" = "Mick",
+        "band" = "Stones",
         row.names = "Mick"
     )
     expect_identical(object, expected)
@@ -78,59 +78,59 @@ context("join : bioinfo")
 
 test_that("Matched rows", {
     df1 <- DataFrame(
-        id = as.factor(seq(4L)),
-        genotype = as.factor(rep(x = c("wt", "ko"), each = 2L))
+        "id" = as.factor(seq(4L)),
+        "genotype" = as.factor(rep(x = c("wt", "ko"), each = 2L))
     )
     df2 <- DataFrame(
-        id = as.factor(seq(4L)),
-        treatment = as.factor(rep(x = c("control", "expt"), times = 2L))
+        "id" = as.factor(seq(4L)),
+        "treatment" = as.factor(rep(x = c("control", "expt"), times = 2L))
     )
     expect_identical(
         object = leftJoin(df1, df2, by = "id"),
         expected = DataFrame(
-            id = as.factor(seq(4L)),
-            genotype = as.factor(rep(x = c("wt", "ko"), each = 2L)),
-            treatment = as.factor(rep(x = c("control", "expt"), times = 2L))
+            "id" = as.factor(seq(4L)),
+            "genotype" = as.factor(rep(x = c("wt", "ko"), each = 2L)),
+            "treatment" = as.factor(rep(x = c("control", "expt"), times = 2L))
         )
     )
 })
 
 test_that("Unmatched rows", {
     df1 <- DataFrame(
-        id = as.factor(seq(4L)),
-        genotype = as.factor(rep(x = c("wt", "ko"), each = 2L))
+        "id" = as.factor(seq(4L)),
+        "genotype" = as.factor(rep(x = c("wt", "ko"), each = 2L))
     )
     df2 <- DataFrame(
-        id = as.factor(seq(4L)),
-        treatment = as.factor(rep(x = c("control", "expt"), times = 2L))
+        "id" = as.factor(seq(4L)),
+        "treatment" = as.factor(rep(x = c("control", "expt"), times = 2L))
     )
     ## Reverse the row order of df2.
     df2 <- df2[rev(seq_len(nrow(df2))), ]
     expect_identical(
         object = leftJoin(df1, df2, by = "id"),
         expected = DataFrame(
-            id = as.factor(seq(4L)),
-            genotype = as.factor(rep(x = c("wt", "ko"), each = 2L)),
-            treatment = as.factor(rep(x = c("control", "expt"), times = 2L))
+            "id" = as.factor(seq(4L)),
+            "genotype" = as.factor(rep(x = c("wt", "ko"), each = 2L)),
+            "treatment" = as.factor(rep(x = c("control", "expt"), times = 2L))
         )
     )
 })
 
 test_that("Uneven rows", {
     df1 <- DataFrame(
-        id = as.factor(seq(4L)),
-        genotype = as.factor(rep(x = c("wt", "ko"), each = 2L))
+        "id" = as.factor(seq(4L)),
+        "genotype" = as.factor(rep(x = c("wt", "ko"), each = 2L))
     )
     df2 <- DataFrame(
-        id = as.factor(seq(2L)),
-        treatment = as.factor(rep(x = c("control", "expt"), times = 1L))
+        "id" = as.factor(seq(2L)),
+        "treatment" = as.factor(rep(x = c("control", "expt"), times = 1L))
     )
     expect_identical(
         object = leftJoin(df1, df2, by = "id"),
         expected = DataFrame(
-            id = as.factor(seq(4L)),
-            genotype = as.factor(rep(x = c("wt", "ko"), each = 2L)),
-            treatment = as.factor(c("control", "expt", NA, NA))
+            "id" = as.factor(seq(4L)),
+            "genotype" = as.factor(rep(x = c("wt", "ko"), each = 2L)),
+            "treatment" = as.factor(c("control", "expt", NA, NA))
         )
     )
 })
