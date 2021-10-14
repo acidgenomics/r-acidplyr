33a5b720cefd5e87224574891d8eb8907ce2309a
Michael Steinbaugh <mike@steinbaugh.com>
2021-10-14 11:49:54 -0400

    Harden against DFrame class

--- tests/testthat/test-mutate.R
+++ tests/testthat/test-mutate.R
@@ -11,46 +11,11 @@ test_that("mutateAll", {
     expect_true(hasRownames(x))
 })
 
-## FIXME Why did I disable this unit test again?
-## Either re-enable or delete.
-
-## > test_that("mutateAll : List column", {
-## >     object <- DataFrame(a = I(list(c(1L, 2L), c(3L, 4L))))
-## >     expect_s4_class(object, "DFrame")
-## >     expect_identical(
-## >         object = dim(object),
-## >         expected = c(2L, 1L)
-## >     )
-## >     expect_identical(
-## >         object = mutateAll(object, fun = toString),
-## >         expected = DataFrame(a = c("1, 2", "3, 4"))
-## >     )
-## > })
-
 test_that("mutateAt", {
     x <- mutateAt(x, vars = c("mpg", "cyl"), log, base = 2L)
     expect_s4_class(x, "DFrame")
     expect_true(hasRownames(x))
 })
-
-## > test_that("mutateAt : List column", {
-## >     object <- DataFrame(
-## >         a = I(list(c(1L, 2L), c(3L, 4L))),
-## >         b = I(list(NULL, NULL))
-## >     )
-## >     expect_s4_class(object, "DFrame")
-## >     expect_identical(
-## >         object = dim(object),
-## >         expected = c(2L, 2L)
-## >     )
-## >     expect_identical(
-## >         object = mutateAt(object, vars = "a", fun = toString),
-## >         expected = DataFrame(
-## >             a = c("1, 2", "3, 4"),
-## >             b = I(list(NULL, NULL))
-## >         )
-## >     )
-## > })
 
 test_that("mutateIf", {
     x <- mutateIf(x, predicate = is.double, fun = as.integer)
