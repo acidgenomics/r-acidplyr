73bc9372f61b38110bc70d21a2cc768ff2e746ac
Michael Steinbaugh <mike@steinbaugh.com>
2022-02-07 13:12:54 -0500

    Update NAMESPACE and dependencies

--- R/AllGenerics.R
+++ R/AllGenerics.R
@@ -1,118 +1,115 @@
 #' @rdname collapseToString
 #' @name collapseToString
-#' @importFrom AcidGenerics collapseToString
 #' @usage collapseToString(object, ...)
 #' @export
 NULL
 
+
+
 #' @rdname join
 #' @name innerJoin
-#' @importFrom AcidGenerics innerJoin
 #' @usage innerJoin(x, y, by, ...)
 #' @export
 NULL
 
 #' @rdname join
 #' @name leftJoin
-#' @importFrom AcidGenerics leftJoin
 #' @usage leftJoin(x, y, by, ...)
 #' @export
 NULL
 
 #' @rdname join
 #' @name rightJoin
-#' @importFrom AcidGenerics rightJoin
 #' @usage rightJoin(x, y, by, ...)
 #' @export
 NULL
 
 #' @rdname join
 #' @name fullJoin
-#' @importFrom AcidGenerics fullJoin
 #' @usage fullJoin(x, y, by, ...)
 #' @export
 NULL
 
 #' @rdname join
 #' @name semiJoin
-#' @importFrom AcidGenerics semiJoin
 #' @usage semiJoin(x, y, by, ...)
 #' @export
 NULL
 
 #' @rdname join
 #' @name antiJoin
-#' @importFrom AcidGenerics antiJoin
 #' @usage antiJoin(x, y, by, ...)
 #' @export
 NULL
 
+
+
 #' @rdname mapToDataFrame
 #' @name mapToDataFrame
-#' @importFrom AcidGenerics mapToDataFrame
 #' @usage mapToDataFrame(x, ...)
 #' @export
 NULL
 
+
+
 #' @rdname melt
 #' @name melt
-#' @importFrom AcidGenerics melt
 #' @usage melt(object, ...)
 #' @export
 NULL
 
+
+
 #' @rdname mutate
 #' @name mutateAll
-#' @importFrom AcidGenerics mutateAll
 #' @usage mutateAll(object, fun, ...)
 #' @export
 NULL
 
 #' @rdname mutate
 #' @name mutateAt
-#' @importFrom AcidGenerics mutateAt
 #' @usage mutateAll(object, fun, ...)
 #' @export
 NULL
 
 #' @rdname mutate
 #' @name mutateIf
-#' @importFrom AcidGenerics mutateIf
 #' @usage mutateIf(object, predicate, fun, ...)
 #' @export
 NULL
 
 #' @rdname mutate
 #' @name transmuteAt
-#' @importFrom AcidGenerics transmuteAt
 #' @usage transmuteAt(object, vars, fun, ...)
 #' @export
 NULL
 
 #' @rdname mutate
 #' @name transmuteIf
-#' @importFrom AcidGenerics transmuteIf
 #' @usage transmuteIf(object, predicate, fun, ...)
 #' @export
 NULL
 
+
+
 #' @rdname rbindToDataFrame
 #' @name rbindToDataFrame
-#' @importFrom AcidGenerics rbindToDataFrame
 #' @usage rbindToDataFrame(x, ...)
 #' @export
 NULL
 
+
+
 #' @rdname select
 #' @name selectIf
-#' @importFrom AcidGenerics selectIf
 #' @usage selectIf(object, predicate, ...)
 #' @export
 NULL
 
+
+
 #' @rdname splitByLevel
 #' @name splitByLevel
-#' @importFrom AcidGenerics splitByLevel
 #' @usage splitByLevel(x, f, ref = FALSE, ...)
 #' @export
 NULL
--- R/package.R
+++ R/package.R
@@ -4,21 +4,4 @@
 #' as DataFrame.
 #'
 #' @keywords internal
-#'
-#' @importClassesFrom BiocGenerics AsIs
-#' @importClassesFrom S4Vectors DataFrame
-#'
-#' @importMethodsFrom pipette as.DataFrame
-#'
-#' @importFrom AcidBase requireNamespaces
-#' @importFrom AcidCLI alert alertInfo
-#' @importFrom AcidGenerics as.DataFrame
-#' @importFrom BiocGenerics as.data.frame cbind do.call lapply order rbind
-#'   rowSums setdiff unique
-#' @importFrom S4Vectors DataFrame SimpleList decode expand.grid merge metadata
-#'   metadata<- na.omit
-#' @importFrom goalie allAreAtomic areDisjointSets assert bapply hasColnames
-#'   hasDims hasLength hasNames hasNoDuplicates hasRownames isAny isCharacter
-#'   isFlag isInt isNumber isScalar isScalarAtomic isString isSubset
-#' @importFrom methods as is setMethod signature
 "_PACKAGE"
