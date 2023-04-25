75ef2a0ea16aa97c2450f5b1e31e3f7bacfb2b8e
Michael Steinbaugh <mike@steinbaugh.com>
2023-04-25 17:34:28 -0400

    NAMESPACE fix for breaking change in Bioc 3.17

--- R/imports.R
+++ R/imports.R
@@ -10,8 +10,8 @@ NULL
 #' @importFrom AcidGenerics antiJoin cast collapseToString fullJoin innerJoin
 #' leftJoin melt mutateAll mutateAt mutateIf rbindToDataFrame rightJoin selectIf
 #' semiJoin splitByLevel transmuteAt transmuteIf
-#' @importFrom BiocGenerics as.data.frame cbind do.call lapply order rowSums
-#' setdiff unique unlist
+#' @importFrom BiocGenerics as.data.frame cbind do.call lapply order setdiff
+#' unique unlist
 #' @importFrom S4Vectors complete.cases decode expand.grid merge metadata
 #' metadata<- na.omit
 NULL
