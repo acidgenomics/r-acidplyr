581322ddbf4f3025072123d3e5c010ce6a411cfd
Michael Steinbaugh <mike@steinbaugh.com>
2023-04-26 16:28:34 -0400

    Rename DataFrame to DFrame

--- R/package.R
+++ R/package.R
@@ -1,7 +1,7 @@
 #' AcidPlyr
 #'
 #' A dplyr-like grammar for manipulation of S4 rectangular data classes, such
-#' as DataFrame.
+#' as `DFrame`.
 #'
 #' @keywords internal
 "_PACKAGE"
--- tests/testthat/test-mutate.R
+++ tests/testthat/test-mutate.R
@@ -1,9 +1,9 @@
-x <- as(mtcars, "DataFrame")
+x <- as(mtcars, "DFrame")
 
 test_that("mutateAll", {
     expect_identical(x[["gear"]][1L], 4) # nolint
     x <- mutateAll(x, fun = log, base = 2L)
-    expect_s4_class(x, "DataFrame")
+    expect_s4_class(x, "DFrame")
     ## Check that log2 calculation was applied correctly.
     expect_identical(x[["gear"]][1L], 2) # nolint
     expect_true(hasRownames(x))
@@ -11,24 +11,24 @@ test_that("mutateAll", {
 
 test_that("mutateAt", {
     x <- mutateAt(x, vars = c("mpg", "cyl"), log, base = 2L)
-    expect_s4_class(x, "DataFrame")
+    expect_s4_class(x, "DFrame")
     expect_true(hasRownames(x))
 })
 
 test_that("mutateIf", {
     x <- mutateIf(x, predicate = is.double, fun = as.integer)
-    expect_s4_class(x, "DataFrame")
+    expect_s4_class(x, "DFrame")
     expect_true(hasRownames(x))
 })
 
 test_that("transmuteAt", {
     x <- transmuteAt(x, vars = c("mpg", "cyl"), log, base = 2L)
-    expect_s4_class(x, "DataFrame")
+    expect_s4_class(x, "DFrame")
     expect_true(hasRownames(x))
 })
 
 test_that("transmuteIf", {
     x <- transmuteIf(x, predicate = is.double, fun = as.integer)
-    expect_s4_class(x, "DataFrame")
+    expect_s4_class(x, "DFrame")
     expect_true(hasRownames(x))
 })
