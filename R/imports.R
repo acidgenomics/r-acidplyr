73f985b2a6309a18426759bd4c5a0b6ebff81527
Michael Steinbaugh <mike@steinbaugh.com>
2023-08-23 15:45:07 -0400

    Pure base R support for unnest2, not requiring tidyr

--- R/imports.R
+++ R/imports.R
@@ -10,10 +10,10 @@ NULL
 #' @importFrom AcidGenerics antiJoin cast collapseToString fullJoin innerJoin
 #' leftJoin melt mutateAll mutateAt mutateIf rbindToDataFrame rightJoin selectIf
 #' semiJoin splitByLevel transmuteAt transmuteIf unnest2
-#' @importFrom BiocGenerics as.data.frame cbind do.call lapply order setdiff
-#' unique unlist
+#' @importFrom BiocGenerics as.data.frame cbind do.call lapply order rbind
+#' setdiff unique unlist
 #' @importFrom S4Vectors complete.cases decode expand.grid merge metadata
-#' metadata<- na.omit
+#' metadata<- na.omit split
 NULL
 
 
@@ -22,7 +22,8 @@ NULL
 
 #' @importFrom S4Vectors DataFrame
 #' @importFrom goalie allAreAtomic areDisjointSets assert bapply hasColnames
-#' hasDims hasLength hasNoDuplicates hasRownames isAny isCharacter isFlag
-#' isInstalled isInt isNumber isScalar isString isSubset requireNamespaces
+#' hasDims hasLength hasNoDuplicates hasRownames hasRows isAny isCharacter
+#' isFlag isInstalled isInt isNumber isScalar isString isSubset
+#' requireNamespaces
 #' @importFrom methods as is new setMethod signature
 NULL
